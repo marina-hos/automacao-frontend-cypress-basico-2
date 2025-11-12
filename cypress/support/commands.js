b95ec23bfd7f1fdc6d28f91b76361262ea78df7a
Marina Hortência <80637846+marinahortencia@users.noreply.github.com>
2025-11-11 22:32:43 -0300

    Update commands file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,21 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+// Comando para abrir a calculadora
+Cypress.Commands.add('abrirCalculadora', () => {
+  cy.visit('/BasicCalculator')
+})
+
+// Comando para preencher os campos e realizar uma operação
+Cypress.Commands.add('calcular', (n1, n2, operacao) => {
+  cy.get('#number1Field').clear().type(n1)
+  cy.wait(200)
+  cy.get('#number2Field').clear().type(n2)
+  cy.wait(200)
+  cy.get('#selectOperationDropdown').select(operacao)
+  cy.wait(200)
+  cy.get('#calculateButton').click()
+  cy.wait(200)
+})
+
+// Comando para verificar o resultado
+Cypress.Commands.add('verificarResultado', (valorEsperado) => {
+  cy.get('#numberAnswerField').should('have.value', valorEsperado)
+})

a1067949c3b951a0644e7d84c6680920c0366d47
Marina Hortência <80637846+marinahortencia@users.noreply.github.com>
2025-11-11 22:56:24 -0300

    Definition of main settings

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,24 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: true,
+    json: false
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("after:screenshot", (details) => {
+        console.log("Screenshot taken: ", details.path);
+      });
+      return config;
     },
-  },
+    video: true,
+    screenshotOnRunFailure: true,
+    baseUrl: "https://testsheepnz.github.io",
+    viewportWidth: 1366,   // largura do navegador
+    viewportHeight: 768    // altura do navegador
+  }
 });
